61fc95bfc1289dc33bc4d1af885e6964a5fd86bf
jnrpurin <jnrpurin@gmail.com>
2020-02-09 18:26:35 -0300

    pom.xml

---
+++ src/main/java/com/wheter/ApiNasa/ApiNasaApplication.java
@@ -0,0 +1,13 @@
+package com.wheter.ApiNasa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiNasaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiNasaApplication.class, args);
+	}
+
+}
